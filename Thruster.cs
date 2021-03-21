7f9ab42232345f6498b0740770f2a8e1fbb3887a
Semen <sammyhuman@mail.ru>
2021-03-21 13:48:55 +0300

    Home work 1

---
+++ Thruster.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Thruster : MonoBehaviour
+{
+    float thrust = 100;
+    Rigidbody thruster;
+
+    private void Start()
+    {
+        thruster = gameObject.GetComponent<Rigidbody>();
+    }
+    public void Thrust(float signalThrust)
+    {
+        thruster.AddForce(transform.up * thrust * signalThrust);
+    }
+}
